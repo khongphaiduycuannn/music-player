5fccdbd9d31d0515e6ad142951241cc99298f420
Duy Cuan <ndmquan1010@gmail.com>
2024-08-02 16:01:31 +0700

    Update data source

--- app/src/main/java/com/notmyexample/musicplayer/data/data_source/SongInMemoryDataSource.kt
+++ app/src/main/java/com/notmyexample/musicplayer/data/data_source/SongInMemoryDataSource.kt
@@ -15,7 +15,7 @@ class SongInMemoryDataSource(
         return listOf(
             Song(1, "Mèo hoang", "Ngọt", 250000, defaultThumbnailList[0], Uri.EMPTY, false, 1),
             Song(2, "Mai mình xa", "Thịnh Suy", 250000, defaultThumbnailList[1], Uri.EMPTY, false, 2),
-            Song(3, "Phút đầu", "Vũ.", 250000, defaultThumbnailList[2], Uri.EMPTY, false, 3),
+            Song(3, "Phút ban đầu", "Vũ.", 250000, defaultThumbnailList[2], Uri.EMPTY, false, 3),
         )
     }
 
@@ -23,7 +23,7 @@ class SongInMemoryDataSource(
         return listOf(
             Album(1, "Tuyển tập nhạc Ngot sôi động 2019", "Ngot", defaultThumbnailList[3], 1),
             Album(2, "Bài hát hay nhất của Thịnh Suy", "Thịnh Suy", defaultThumbnailList[4], 1),
-            Album(3, "Bài hát hay nhất của Vũ", "Vũ,", defaultThumbnailList[5], 1)
+            Album(3, "Bài hát hay nhất của Vũ", "Vũ.", defaultThumbnailList[5], 1)
         )
     }
 
